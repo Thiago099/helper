773f3f31ae3ed7247336719fa3d8fac03da162d9
Thiago099 <>
2020-12-18 14:15:44 -0300

    [feature] semi-full querry

--- index.php
+++ index.php
@@ -92,5 +92,45 @@
       }
       ?>
     </textarea>
+    <textarea name="name" rows="40" cols="200"><?php
+    if(isset($_GET['database'])&&isset($_GET['table']))
+    {
+      function loop($database,$table,&$join,&$select)
+      {
+        $info=new sql("information_schema");
+        $db=new sql($database);
+        $fks=$info->query("SELECT K.COLUMN_NAME coluna,k.REFERENCED_TABLE_NAME tabela, k.REFERENCED_COLUMN_NAME chave
+        FROM information_schema.TABLE_CONSTRAINTS i
+        LEFT JOIN information_schema.KEY_COLUMN_USAGE k ON i.CONSTRAINT_NAME = k.CONSTRAINT_NAME
+        WHERE i.CONSTRAINT_TYPE = 'FOREIGN KEY'
+        AND i.TABLE_SCHEMA = '$database'
+        AND i.TABLE_NAME = '$table'
+        GROUP BY coluna;");
+        $table=$_GET['table'];
+        $fields="";
+        foreach ($fks as $i) {
+            $ct=$i['tabela'];
+            $fields=$db->query("DESC $ct");
+            foreach ($fields as $j) {
+              $jj=$j['Field'];
+              if($i['chave']==$jj)continue;
+              $select.="{$table}_$i[coluna].$jj AS {$jj}_$ct,\n";
+            }
+            $join.="LEFT JOIN $ct AS {$table}_$i[coluna] ON $table.$i[coluna] = {$table}_$i[coluna].$i[chave]\n";
+        }
+/*
+        foreach ($fks as $i) {
+            loop($database,$i['tabela'],$join,$select);
+        }*/
+
+      }
+      $join='';
+      $select=",\n";
+      loop($_GET['database'],$_GET['table'],$join,$select);
+      if($select==",\n")$select='';
+      $select=substr($select, 0, -2)."\n";
+      echo "SELECT\n$_GET[table].*{$select}FROM $_GET[table]\n$join";
+    }
+    ?></textarea>
   </body>
 </html>
