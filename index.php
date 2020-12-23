478c28d275a3abdf7fdde4c1a48250247266f6a6
Thiago099 <>
2020-12-23 13:50:43 -0300

    [fix] gerar campos funciona mesmo se alguns campos ainda existierm

--- index.php
+++ index.php
@@ -48,15 +48,18 @@
         $table = $_GET['table'];
         $db=new sql($database);
         $result=$db->run("ALTER TABLE `$database`.`$table`
-              ADD COLUMN `id` INT NOT NULL AUTO_INCREMENT,
-              ADD COLUMN `created_by` INT NULL,
-              ADD COLUMN `created_at` DATETIME NULL,
-              ADD COLUMN `updated_by` INT NULL,
-              ADD COLUMN `updated_at` DATETIME NULL,
-              ADD COLUMN `excluido` TINYINT(1) NULL DEFAULT NULL,
-              ADD PRIMARY KEY (`id`),
-              ADD CONSTRAINT `FK_{$table}_created_by` FOREIGN KEY (`created_by`) REFERENCES `usuario` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION,
-              ADD CONSTRAINT `FK_{$table}_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `usuario` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION;");
+                          ADD COLUMN IF NOT EXISTS `created_by` INT NULL,
+                          ADD COLUMN IF NOT EXISTS `created_at` DATETIME NULL,
+                          ADD COLUMN IF NOT EXISTS `updated_by` INT NULL,
+                          ADD COLUMN IF NOT EXISTS `updated_at` DATETIME NULL,
+                          ADD COLUMN IF NOT EXISTS `excluido` TINYINT(1) NULL DEFAULT NULL;");
+        $result=$db->run("ALTER TABLE `$database`.`$table`
+                            ADD COLUMN IF NOT EXISTS `id` INT NOT NULL AUTO_INCREMENT,
+                            ADD PRIMARY KEY (`id`);");
+        $result=$db->run("ALTER TABLE `$database`.`$table`
+                            ADD CONSTRAINT `FK_{$table}_created_by` FOREIGN KEY (`created_by`) REFERENCES `usuario` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION;");
+        $result=$db->run("ALTER TABLE `$database`.`$table`
+                            ADD CONSTRAINT `FK_{$table}_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `usuario` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION;");
       }
       ?>
       </form>
